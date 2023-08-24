15322e24b14b5c3c59c659bcd0c9eb1cbb089b88
Adam Perkins <adammpkins@gmail.com>
2023-08-24 00:08:22 +0000

    removed bad workflows

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -12,5 +12,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    base: '/llamaterm.github.io/'
 })
